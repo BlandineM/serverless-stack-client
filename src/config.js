b9439d718f06cebb2022638cca0bfe5d7c46e21f
Blandine <blandine.mallart@gmail.com>
2020-02-12 10:08:47 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "note-app-b"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://c2lu1i01nb.execute-api.us-east-2.amazonaws.com"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_hEr7EbaMr",
+    APP_CLIENT_ID: "2r0a385h7911k3h2cclf8nd0ea",
+    IDENTITY_POOL_ID: "us-east-2:995e8850-9f51-431f-a533-53e4cf3e821a"
+  }
+};
